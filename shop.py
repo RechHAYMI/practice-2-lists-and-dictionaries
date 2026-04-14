f5b8c4237407626a422810103f92837ac94a6737
RechHAYMI <sasha.rumyantsev.14@bk.ru>
2026-04-14 21:14:45 +0300

    feat: added dynamic inventory management with alerts

---
+++ shop.py
@@ -0,0 +1,17 @@
+products = [
+    {"name": "Молоко", "price": "80", "stock": "10"},
+    {"name": "Хлеб", "price": "50", "stock": "30"},
+    {"name": "Сникерс", "price": "150", "stock": "20"}
+]
+
+products.append({"name": "Кола", "price": "100", "stock": "5"})
+
+total_money = 0
+
+for product in products:
+    print(f"Товар: {product['name']}, Цена: {product['price']}, Количество: {product['stock']}.")
+    if int(product["stock"]) < 15:
+        print(f"⚠️ ВНИМАНИЕ: {product['name']} заканчивается! осталось всего {(product['stock'])} штук. ")
+    current_sum = int(product["price"]) * int(product["stock"])
+    total_money += current_sum
+print(total_money)    
