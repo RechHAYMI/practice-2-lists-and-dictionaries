1af7b5e28173d275b1ec3f8c7b1bd0da94e5ae34
RechHAYMI <sasha.rumyantsev.14@bk.ru>
2026-04-15 16:41:20 +0300

    added interactive functions and error protection

--- shop.py
+++ shop.py
@@ -9,12 +9,30 @@ products.append({"name": "Кола", "price": "100", "stock": "5"})
 bad_snikers = {"name": "Сникерс", "price": "150", "stock": "20"}
 products.remove(bad_snikers)
 
-total_money = 0
+def show_all_products():
+    total_money = 0
+    print(f"--- В БАЗЕ СЕЙЧАС {len(products)} товаров ---. ")
+    for product in products:
+        print(f"Товар: {product['name']}, Цена: {product['price']}, Количество: {product['stock']}.")
+        if int(product["stock"]) < 15:
+            print(f"⚠️ ВНИМАНИЕ: {product['name']} заканчивается! осталось всего {(product['stock'])} штук. ")
+        try:
+            current_sum = int(product["price"]) * int(product["stock"])
+            total_money += current_sum
+        except:
+            print("Ошибка: цена или количество должны быть числами!")
+            continue
+    print(total_money)
 
-for product in products:
-    print(f"Товар: {product['name']}, Цена: {product['price']}, Количество: {product['stock']}.")
-    if int(product["stock"]) < 15:
-        print(f"⚠️ ВНИМАНИЕ: {product['name']} заканчивается! осталось всего {(product['stock'])} штук. ")
-    current_sum = int(product["price"]) * int(product["stock"])
-    total_money += current_sum
-print(total_money)    
+def add_new_product(name, price, stock):
+    products.append({"name": name, "price": price, "stock": stock})
+
+while True:
+    new_name = input("Введите название: ")
+    if new_name == "стоп":
+        break
+    new_price = input("Введите цену: ")
+    new_stock = input("Введите количество: ")
+
+    add_new_product(new_name, new_price, new_stock)
+    show_all_products()
